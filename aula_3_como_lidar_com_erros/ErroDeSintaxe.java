a9a73fbba27314166abbb9fe1c8de84c7f97f58f
Marcelo <joao69@aluno.senai-ce.org.br>
2026-02-21 09:12:29 -0300

    adicionando erro de sintaxe

---
+++ aula_3_como_lidar_com_erros/ErroDeSintaxe.java
@@ -0,0 +1,10 @@
+class ErroDeSintaxe {
+	
+	public static void main (String[] args){
+
+		System.out.println("errando?)
+	}
+}
+
+//se reparar bem, tirei o (") e o (;).
+//o Java sendo case sensitive
