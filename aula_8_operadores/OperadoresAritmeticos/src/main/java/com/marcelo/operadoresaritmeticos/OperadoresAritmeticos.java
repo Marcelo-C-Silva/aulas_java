3697a3842817257daf1e7dd82e13aec6ecbc1c5f
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-01 13:18:20 -0300

    adicionando exemplo de soma

---
+++ aula_8_operadores/OperadoresAritmeticos/src/main/java/com/marcelo/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,12 @@
+
+package com.marcelo.operadoresaritmeticos;
+
+
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        int soma = 1 + 2;
+        
+        System.out.println(soma);
+    }
+}
