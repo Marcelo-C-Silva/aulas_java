e0032b06788200d80c954c6892dfc15282008e1f
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-03 10:02:13 -0300

    adicionando atividade basica de calculo de media

---
+++ atividades/NotasBimestre/src/main/java/com/carneiro/notasbimestre/NotasBimestre.java
@@ -0,0 +1,25 @@
+
+package com.carneiro.notasbimestre;
+
+import java.util.Scanner;
+
+public class NotasBimestre {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Vamos calcular sua media bimestral: ");
+        
+        System.out.println("Digite sua primeira nota:");
+        double nota1 = scan.nextDouble();
+        
+        System.out.println("Digite sua segunda nota: ");
+        double nota2 = scan.nextDouble();
+        
+        System.out.println("Digite sua terceira nota: ");
+        double nota3 = scan.nextDouble();
+        
+        
+        double resultado = (nota1 + nota2 + nota3)/3;
+        System.out.printf("Sua nota parcial é: %.2f", resultado);
+    }
+}
