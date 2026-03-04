30b28d9e0b6b2737cdb6ef5d14f2105e181bd429
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-04 08:34:09 -0300

    adicionando atividade de logica calcular area do quadrado

---
+++ atividades/AreaDoQuadrado/src/main/java/com/carneiro/areadoquadrado/AreaDoQuadrado.java
@@ -0,0 +1,23 @@
+package com.carneiro.areadoquadrado;
+
+import java.util.Scanner;
+
+public class AreaDoQuadrado {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Vamos calcular a área de um quadrado");
+        System.out.println("Digite o numero:");
+        double lado = scan.nextDouble();
+        double area = Math.pow(lado, 2);
+        
+        System.out.println("O valor da área do seu quadrado é: " + area + "cm²");
+        
+        double dobroDaArea = area*2;
+        System.out.println("Esse é o dobro da sua área: " + dobroDaArea);
+        
+        
+        
+    }
+}
