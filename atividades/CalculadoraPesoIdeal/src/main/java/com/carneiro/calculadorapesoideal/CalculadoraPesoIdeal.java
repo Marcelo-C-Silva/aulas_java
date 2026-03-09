604eac8ae6458413b2007918e0df6a24a6eb01b9
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-09 10:22:49 -0300

    refatoradno atividade calculadora de imc

--- atividades/CalculadoraPesoIdeal/src/main/java/com/carneiro/calculadorapesoideal/CalculadoraPesoIdeal.java
+++ atividades/CalculadoraPesoIdeal/src/main/java/com/carneiro/calculadorapesoideal/CalculadoraPesoIdeal.java
@@ -7,12 +7,30 @@ public class CalculadoraPesoIdeal {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
+        System.out.println("Digite seu nome");
+        String nome = scan.next();
         
-        System.out.println("Digite sua altura para que possamos fazer o calculo do seu peso ideal");
+        System.out.println("Digite seu peso");
+        double peso = scan.nextDouble();
+        
+        System.out.println("Digite sua altura");
         double altura = scan.nextDouble();
         
-        double formulaHomem = (72.7*altura) - 58;
-        double formulaMulher = (62.1*altura) - 44.7;
+        double imc = peso /(altura*altura);
+        
+        if(imc < 18.5){
+            System.out.println(nome+ ", seu IMC é: " + imc);
+            System.out.println("Abaixo do peso");
+        }else if(imc<=24.9){
+            System.out.println(nome+ ", seu IMC é: " + imc);
+            System.out.println("Peso normal");
+        }else if(imc<=29.9){
+            System.out.println(nome+ ", seu IMC é: " + imc);
+            System.out.println("Sobrepeso");
+        }else{
+            System.out.println(nome+ ", seu IMC é: " + imc);
+            System.out.println("Obesidade");
+        }
         
   
     }
