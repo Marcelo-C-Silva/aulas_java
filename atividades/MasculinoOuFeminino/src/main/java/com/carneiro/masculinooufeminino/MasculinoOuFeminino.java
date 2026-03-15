72b388059788c1be8a5c7fc7b4160e278785eb0a
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-15 14:28:17 -0300

    adicionando atividade de logica usando if else

---
+++ atividades/MasculinoOuFeminino/src/main/java/com/carneiro/masculinooufeminino/MasculinoOuFeminino.java
@@ -0,0 +1,23 @@
+
+package com.carneiro.masculinooufeminino;
+
+import java.util.Scanner;
+
+public class MasculinoOuFeminino {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite F ou M \nF-Feminino\nM-Masculino");
+        
+        char letra = scan.next().charAt(0); 
+        
+        if(letra == 'F'){
+            System.out.println("Feminino");
+        }else if(letra == 'M'){
+            System.out.println("Masculino");
+        }else{
+            System.out.println("Indeterminado");
+        }
+        
+    }
+}
