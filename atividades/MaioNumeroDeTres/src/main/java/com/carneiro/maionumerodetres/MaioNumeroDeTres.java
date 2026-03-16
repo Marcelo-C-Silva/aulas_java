eb8e32fb1f5847e4b870134201ac549ff6db8f2a
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-16 10:15:15 -0300

    refatorando codigo para adicionar outra logica de menor numero

--- atividades/MaioNumeroDeTres/src/main/java/com/carneiro/maionumerodetres/MaioNumeroDeTres.java
+++ atividades/MaioNumeroDeTres/src/main/java/com/carneiro/maionumerodetres/MaioNumeroDeTres.java
@@ -1,16 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.carneiro.maionumerodetres;
 
-/**
- *
- * @author kirob
- */
+import java.util.Scanner;
+
 public class MaioNumeroDeTres {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Escreva o Primeiro numero");
+        double numero1 = scan.nextDouble();
+        
+        System.out.println("Escreva o Segundo numero");
+        double numero2 = scan.nextDouble();
+        
+        System.out.println("Escreva o Terceiro numero");
+        double numero3 = scan.nextDouble();
+        
+        double maior = numero1;
+        double menor = numero1;
+        
+        if(numero2 > maior){
+            maior = numero2;
+        }
+        if(numero3 > maior){
+            maior = numero3;
+        }
+        if(numero2 < menor){
+            menor = numero2;
+        }
+        if(numero3 < menor){
+            menor = numero3;
+        }
+        System.out.println("O maior numero dos 3 é: " + maior);
+        System.out.println("O menor numero dos 3 é: " + menor);
+
     }
 }
