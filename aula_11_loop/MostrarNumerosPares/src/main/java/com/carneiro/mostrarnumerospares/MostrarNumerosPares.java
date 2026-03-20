37aca7ac2ae361b6bbfd65af5b1dc54521f73120
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-20 09:29:15 -0300

    adicionando atividade de loop

---
+++ aula_11_loop/MostrarNumerosPares/src/main/java/com/carneiro/mostrarnumerospares/MostrarNumerosPares.java
@@ -0,0 +1,15 @@
+
+package com.carneiro.mostrarnumerospares;
+
+public class MostrarNumerosPares {
+
+    public static void main(String[] args) {
+        int numero = 2;
+        
+        while (numero <= 50){
+            System.out.println(numero);
+            numero += 2;
+        }
+        
+    }
+}
