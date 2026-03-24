9c10d2057ea1c8867d07c0fcc75db67643a4b770
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-24 08:52:59 -0300

    adicionando atividade de loop for

---
+++ aula_12_loop_for/ForDecrescente/src/main/java/com/carneiro/fordecrescente/ForDecrescente.java
@@ -0,0 +1,13 @@
+
+
+package com.carneiro.fordecrescente;
+
+
+public class ForDecrescente {
+
+    public static void main(String[] args) {
+        for (int count = 10;  count >= 1; count--){
+            System.out.println(count);
+        }
+    }
+}
