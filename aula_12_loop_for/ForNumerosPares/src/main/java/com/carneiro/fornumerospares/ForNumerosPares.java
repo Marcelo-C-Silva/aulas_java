7933bafa250db27801c5e0aa0ddfb3e4ce79374d
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-26 08:56:59 -0300

    adicionando atividade de logica for

---
+++ aula_12_loop_for/ForNumerosPares/src/main/java/com/carneiro/fornumerospares/ForNumerosPares.java
@@ -0,0 +1,10 @@
+
+package com.carneiro.fornumerospares;
+public class ForNumerosPares {
+
+    public static void main(String[] args) {
+        for (int count = 2; count<=20; count +=2){
+            System.out.println(count);
+        }
+    }
+}
