0efa2846bf61fbed77940d22dcbefde4226aeb11
Marcelo <joao69@aluno.senai-ce.org.br>
2026-03-26 09:05:54 -0300

    adicionando atividade de logica for

---
+++ aula_12_loop_for/SomarDe1Ate100/src/main/java/com/carneiro/somarde1ate100/SomarDe1Ate100.java
@@ -0,0 +1,15 @@
+
+
+package com.carneiro.somarde1ate100;
+
+
+public class SomarDe1Ate100 {
+
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int count = 1; count <= 100; count++){
+            soma += count;
+        }
+        System.out.println(soma);
+    }
+}
